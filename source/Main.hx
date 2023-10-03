1d7bf89f5b65841c49b5c2967897a281b5ae0981
CharlesCatYT <62410083+CharlesCatYT@users.noreply.github.com>
2023-10-03 16:24:20 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -84,7 +84,7 @@ class Main extends Sprite
 		initialState = TitleState;
 		#end
 
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, /*zoom*/, framerate, framerate, skipSplash, startFullscreen));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, /*zoom,*/ framerate, framerate, skipSplash, startFullscreen));
 
 		setFPS();
 
